8e2d5b627d62a747f29a11ad76fe9aa06ae31b89
Goutham <gouthamdevaraju@gmail.com>
2024-10-30 17:05:18 +0530

    Added initial project

---
+++ SwiftUI-Environment-Post-iOS-17/SwiftUI_Environment_Post_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Environment_Post_iOS_17App.swift
+//  SwiftUI-Environment-Post-iOS-17
+//
+//  Created by Goutham Devaraju on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Environment_Post_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
